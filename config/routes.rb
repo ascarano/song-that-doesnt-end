8638d11c54886ac4e2475548c29ba564e90b0d62
ascarano <adamscarano@gmail.com>
2014-10-14 10:49:29 -0600

    all pages are linked for never ending song

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  get '/' => "pages#index", as: :index1
+
+  get 'verse-2' => 'pages#verse_2', as: :verse2
+
+  get 'verse-3' => 'pages#verse_3', as: :verse3
+
+  get 'verse-4' => 'pages#verse_4', as: :verse4
+
+
+
 end
